1e5da81b0a44a694a140ea3e66d5a09899a1463d
OnSebii <lang.sebastian.006@gmail.com>
2024-10-16 15:01:26 +0200

    Update CustomerAccountManagementSteps.java

--- src/test/java/stepdefinitions/CustomerAccountManagementSteps.java
+++ src/test/java/stepdefinitions/CustomerAccountManagementSteps.java
@@ -21,15 +21,39 @@ public class CustomerAccountManagementSteps {
 
     @When("I provide my personal details")
     public void iProvideMyPersonalDetails() {
-        
+        customer.setName("Jane Smith");
+        customer.setEmail("jane.smith@example.com");
+        customer.setPassword("SecurePassword123");
+        boolean accountCreated = customer.registerAccount();
+        assertTrue(accountCreated, "Customer account should be created");
     }
 
     @Then("my Customer account is created")
     public void myCustomerAccountIsCreated() {
-        
+        assertNotNull(customer.getCustomerId(), "Customer ID should not be null");
     }
 
     @And("I receive a unique Customer ID")
     public void iReceiveAUniqueCustomerID() {
+        System.out.println("Customer ID: " + customer.getCustomerId());
+    }
+
+    // Scenario 2: Linking payment methods and topping up the Prepaid Account
+    @Given("I am logged into my Customer account")
+    public void iAmLoggedIntoMyCustomerAccount() {
+    }
+
+    @When("I link my payment method")
+    public void iLinkMyPaymentMethod() {
+        
+    }
+
+    @And("I perform a Balance Top-Up of ${int} to my Prepaid Account")
+    public void iPerformABalanceTopUpOf$ToMyPrepaidAccount(int arg0) {
+        
+    }
+
+    @Then("my balance in the Prepaid Account increases by ${int}")
+    public void myBalanceInThePrepaidAccountIncreasesBy$(int arg0) {
     }
 }
